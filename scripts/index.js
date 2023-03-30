19ba9a57500fd21cc680a01ac3f59ea20541d0b1
Rishil patel <rishilpatel157@gmail.com>
2023-03-30 21:45:45 +0530

    added

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const left = document.querySelector("#left-side");
+console.log(left)
+const handleMove = e => {
+  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+}
+
+document.onmousemove = e => handleMove(e);
+
+document.ontouchmove = e => handleMove(e.touches[0]);
